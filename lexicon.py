621d5e4262a8e463dbc59b6b974cfae87ebfa6d9
Valeriya <Dzhavadova.v@yandex.ru>
2024-01-31 14:24:50 +0300

    add dictionary for messages

---
+++ lexicon.py
@@ -0,0 +1,15 @@
+MESSAGE: dict[str, str] = {
+    "at_work": "в работе",
+    "dispatch_question": "\nОтправить файлы поставщикам? да/нет\n",
+    "confirm_answers": ("lf", "yes", "нуы", "да"),
+    "text_for_mail_question": "\nВведите текст письма или нажмите enter\nтекст по-умолчанию:",
+    "text_for_mail_default": "'Файл во вложении прошу ознакомиться'",
+    "process_sending": "\nОтправка писем...",
+    "success_sending": "\nПисьма отправлены успешно",
+    "files_save_in": "\nФайлы сохранены:",
+    "file_path_question": "\nВведите путь к файлу:\n",
+    "name_split_question": "\nВведите название к файлу для поставщика:\n",
+    "type_error": "\nДля обработки данных файл должен быть 'xlsx'",
+    "sendmail_error": "\nПроверьте логин и пароль",
+    "": "",
+}
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
 
+from lexicon import MESSAGE
 
-def send_file_to_providers(file_name, to_email, text):
+
+def send_file_to_providers(file_name: str, to_email: str, text: str) -> None:
     sender = ""
     password = ""
     subject = Path(file_name).name
@@ -34,60 +36,66 @@ def send_file_to_providers(file_name, to_email, text):
         server.login(sender, password)
         server.sendmail(sender, to_email, msg.as_string())
     except Exception as _ex:
-        print(f"{_ex}\nПроверьте логин и пароль")
+        print(f'{_ex}{MESSAGE["sendmail_error"]}')
     finally:
         server.quit()
 
 
 def split_file_to_providers(
-    file_name="",
-    name_split="отборка_2024",
-    providers_email="Почта поставщиков.xlsx",
+    file_path: str,
+    name_split: str = "отборка_2024",
+    providers_email: str = "Почта поставщиков.xlsx",
 ):
     """Функция для разделения единного файла для рассылки поставщикам.
-    file_name - имя файла формата .xlsx
-    name_split - название итогового файла для поставщика
+    file_path: путь к файлу формата .xlsx
+    name_split: название итогового файла для поставщика
+    providers_email: путь к файлу с почтой поставщиков
     """
 
-    if Path(file_name).suffix == ".xlsx":
-        print(f"\n{Path(file_name).name} в работе")
+    if Path(file_path).suffix == ".xlsx":
+        print(f'\n{Path(file_path).name} {MESSAGE["at_work"]}')
 
-        full_file = pd.read_excel(Path(file_name).name)
+        full_file = pd.read_excel(Path(file_path).name)
         provider_email = pd.read_excel(providers_email)
         provider_data = full_file.groupby("Поставщик")
-        for_send_mail = Path.cwd() / f"файлы для отправки {name_split}.xlsx"
+        #  удалить или оставить возможность сохранить итог
+        # for_send_mail = Path(file_path).parent / f"файлы для отправки {name_split}.xlsx"
+
+        directory_path = Path(file_path).parent / "providers_file"
+
+        if not directory_path.exists():
+            directory_path.mkdir()
 
         created_files = []
         for provider, data in provider_data:
-            file = Path.cwd() / "providers_file" / f"{provider[3:]}_{name_split}.xlsx"
+            file = directory_path / f"{provider[3:]}_{name_split}.xlsx"
             data.to_excel(file, index=False)
             created_files.append((provider, str(file)))
 
         df = pd.DataFrame(created_files, columns=("Поставщик", "Файл"))
         for_send_data = pd.merge(df, provider_email, how="left", on="Поставщик")
-        # for_send_data.to_excel(for_send_mail, index=False)                        # IF NEED SAVE DATA AS .xlsx
 
-        send_file = input("\nОтправить файлы поставщикам? да/нет\n")
-        if send_file.lower() == "да":
+        send_file = input(MESSAGE["dispatch_question"])
+        if send_file.lower() in MESSAGE["confirm_answers"]:
             text = input(
-                '\nВведите текст письма или нажмите enter\nтекст по-умолчанию:"Файл во вложении прошу ознакомиться":\n'
+                MESSAGE["text_for_mail_question"], MESSAGE["text_for_mail_default"]
             )
             if not text:
-                text = "Файл во вложении прошу ознакомиться"
-            print("\nОтправка писем...")
+                text = MESSAGE["text_for_mail_default"]
+            print(MESSAGE["process_sending"])
             for index, row in for_send_data.iterrows():
                 send_file_to_providers(row["Файл"], row["Почта"], text)
-            print("Письма отправлены успешно")
+            print(MESSAGE["success_sending"])
 
-        return f"\nФайлы сохранены: {Path.cwd() / 'providers_file'}"
+        return f'{MESSAGE["files_save_in"]} {directory_path}'
     else:
-        return f"\nДля обработки данных файл должен быть 'xlsx'"
+        return MESSAGE["type_error"]
 
 
 def main():
-    user_file_name = input("\nВведите название общего файла:\n")
-    user_name_split = input("\nВведите название к файлу для поставщика:\n")
-    split_file_to_providers(file_name=user_file_name, name_split=user_name_split)
+    user_file_path = input(MESSAGE["file_path_question"])
+    user_name_split = input(MESSAGE["name_split_question"])
+    split_file_to_providers(file_path=user_file_path, name_split=user_name_split)
 
 
 if __name__ == "__main__":
