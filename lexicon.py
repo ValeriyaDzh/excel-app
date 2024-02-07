58aad2c002a4ea7b8d4f544596993f0e4d265fa4
Valeriya <Dzhavadova.v@yandex.ru>
2024-02-07 22:26:36 +0300

    add dictionary for columns names

--- lexicon.py
+++ lexicon.py
@@ -3,13 +3,19 @@ MESSAGE: dict[str, str] = {
     "dispatch_question": "\nОтправить файлы поставщикам? да/нет\n",
     "confirm_answers": ("lf", "yes", "нуы", "да"),
     "text_for_mail_question": "\nВведите текст письма или нажмите enter\nтекст по-умолчанию:",
-    "text_for_mail_default": "'Файл во вложении прошу ознакомиться'",
+    "text_for_mail_default": "Файл во вложении прошу ознакомиться",
     "process_sending": "\nОтправка писем...",
     "success_sending": "\nПисьма отправлены успешно",
     "files_save_in": "\nФайлы сохранены:",
     "file_path_question": "\nВведите путь к файлу:\n",
     "name_split_question": "\nВведите название к файлу для поставщика:\n",
+    "providers_path_question": "\nВведите название к файлу с почтой поставщиков:\n",
     "type_error": "\nДля обработки данных файл должен быть 'xlsx'",
     "sendmail_error": "\nПроверьте логин и пароль",
-    "": "",
+}
+
+COLUMNS: dict[str, str] = {
+    "provider": "Поставщик",
+    "file": "Файл",
+    "email": "Почта",
 }
